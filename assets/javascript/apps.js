5db2acd98c3a72c52cfa7b9c572aaa06f9ab405e
Luis Mendoza <luismendoza@Luiss-MacBook-Pro.local>
2017-11-02 19:30:46 -0700

    updated grammar

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -6,16 +6,16 @@ var intervalId;
 var y;
 var r;
 var qNum=0;
-var test = [{ question:"Which games is not a sport video game", choice1:"FIFA 17", choice2:"NBA 2K17", choice3:"Marvel vs Capcom", choice4:"NFL 17", correct: "Marvel vs Capcom", src: "assets/images/mvc.jpg" },
-			{ question:"Which game is about soccer:", choice1:"NBA 2K17", choice2:"FIFA 17", choice3:"Candy Crush", choice4:"NHL 17", correct: "FIFA 17", src:"assets/images/fifa.png" },
-			{ question:"Which game is a fighting game", choice1:"NBA 2K17", choice2:"Call of Duty", choice3:"Overwatch", choice4:"Mortal Kombat", correct: "Mortal Kombat",src: "assets/images/kombat.png" },
-			{ question:"Which game is owned by Activision", choice1:"Overwatch", choice2:"Mario Brothers", choice3:"Call of Duty", choice4:"Battlefield", correct: "Call of Duty", src: "assets/images/cod.jpg" },
-			{ question:"Which is a racing game", choice1:"Mario Kart", choice2:"Dragon Ball Z: Budokai", choice3:"Smash Brothers", choice4:"Lego: Star Wars", correct: "Mario Kart", src: "assets/images/mk.jpg"},	
-			{ question:"Which game is owned by Nintendo", choice1:"Super Smash Brothers", choice2:"Dragon Ball Z: Budokai", choice3:"Smash Brothers", choice4:"Lego: Star Wars", correct: "Super Smash Brothers", src: "assets/images/smash.jpg"},	
-			{ question:"Which game was created by King", choice1:"Call of Duty", choice2:"Mario Party", choice3:"Candy Crush", choice4:"Street Fighter", correct: "Candy Crush", src: "assets/images/candy.jpg"},	
-			{ question:"Which is a classic game", choice1:"Tekken", choice2:"Ms. Pacman", choice3:"Smash Brothers", choice4:"League of Legends", correct: "Ms. Pacman", src: "assets/images/pacman.jpeg"},	
-			{ question:"Which game involves running and jumping", choice1:"Super Mario", choice2:"Tetris", choice3:"Logo Quiz", choice4:"Lego: Star Wars", correct: "Super Mario", src: "assets/images/mario.jpg"},	
-			{ question:"Which game is involves putting blocks together", choice1:"Mario Kart", choice2:"Tetris", choice3:"Pokemon Go", choice4:"Sonic", correct: "Tetris", src: "assets/images/tetris.png"}		
+var test = [{ question:"Which game is not a sport video game?", choice1:"FIFA 17", choice2:"NBA 2K17", choice3:"Marvel vs Capcom", choice4:"NFL 17", correct: "Marvel vs Capcom", src: "assets/images/mvc.jpg" },
+			{ question:"Which game involves soccer?", choice1:"NBA 2K17", choice2:"FIFA 17", choice3:"Candy Crush", choice4:"NHL 17", correct: "FIFA 17", src:"assets/images/fifa.png" },
+			{ question:"Which of the following is a combat game?", choice1:"NBA 2K17", choice2:"Call of Duty", choice3:"Overwatch", choice4:"Mortal Kombat", correct: "Mortal Kombat",src: "assets/images/kombat.png" },
+			{ question:"Which game is owned by Activision?", choice1:"Overwatch", choice2:"Mario Brothers", choice3:"Call of Duty", choice4:"Battlefield", correct: "Call of Duty", src: "assets/images/cod.jpg" },
+			{ question:"Which of the following is a racing game?", choice1:"Mario Kart", choice2:"Dragon Ball Z: Budokai", choice3:"Smash Brothers", choice4:"Lego: Star Wars", correct: "Mario Kart", src: "assets/images/mk.jpg"},	
+			{ question:"Which game is owned by Nintendo?", choice1:"Super Smash Brothers", choice2:"Dragon Ball Z: Budokai", choice3:"Tekken", choice4:"Lego: Star Wars", correct: "Super Smash Brothers", src: "assets/images/smash.jpg"},	
+			{ question:"Which game was developed by King?", choice1:"Call of Duty", choice2:"Mario Party", choice3:"Candy Crush", choice4:"Street Fighter", correct: "Candy Crush", src: "assets/images/candy.jpg"},	
+			{ question:"Which of the following games is a classic?", choice1:"Tekken", choice2:"Ms. Pacman", choice3:"Smash Brothers", choice4:"League of Legends", correct: "Ms. Pacman", src: "assets/images/pacman.jpeg"},	
+			{ question:"Which game does the character Yoshi belong to?", choice1:"Super Mario", choice2:"Tetris", choice3:"Logo Quiz", choice4:"Lego: Star Wars", correct: "Super Mario", src: "assets/images/mario.jpg"},	
+			{ question:"Which game requires the perfect arrangement of blocks to win?", choice1:"Mario Kart", choice2:"Tetris", choice3:"Pokemon Go", choice4:"Sonic", correct: "Tetris", src: "assets/images/tetris.png"}		
 			];
 
 
@@ -90,7 +90,7 @@ function timesUp(){
 	$(".img").attr("src",test[qNum].src);
 	wrong++;
 	qNum++;
-	$(".question").html("<h4>Times up. The correct answer was </h4>");
+	$(".question").html("<h4>Time's up! The correct answer was </h4>");
 	if (qNum===10) {
 		flag=false;
 	}
@@ -137,7 +137,7 @@ function displayQuestion() {
 				timer=20;	
 				$(".timer").html("<h3></h3>");
 				clearInterval(r);		
-				$(".question").html("<h4>WRONG THE CORRECT ANSWER WAS </h4>");
+				$(".question").html("<h4>WRONG! THE CORRECT ANSWER WAS </h4>");
 				$(".img").attr("src",test[qNum].src);
 				wrong++;
 				qNum++;
